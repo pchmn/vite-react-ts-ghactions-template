dec634a996f730e4dffc2c5a1e59953eb3cc3a78
pchmn <pchmn.dev@gmail.com>
2021-11-19 12:46:49 +0100

    test: change structure

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,13 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
-  setupFilesAfterEnv: ['<rootDir>/src/testing/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: '<rootDir>/coverage/',
   coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '(.*).d.ts$'],
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/testing/fileMock.ts',
+    '.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      'jest-transform-stub',
     '^@app/(.*)$': '<rootDir>/src/app/$1'
   }
 };
