64798630f7d19263c8037402ce6d88996f4c8eb6
pchmn <pchmn.dev@gmail.com>
2022-03-11 14:06:04 +0100

    conf: add `prettierrc` file, use js file instead of jon for `eslintrc`

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    jest: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ],
+  plugins: ['react', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:jest/recommended', 'plugin:testing-library/react']
+    }
+  ],
+  rules: {
+    'prettier/prettier': ['error']
+  }
+};
