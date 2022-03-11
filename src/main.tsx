792939c5a99446a22080c97c05096dd17546abac
pchmn <pchmn.dev@gmail.com>
2022-03-11 14:07:40 +0100

    conf: fix auto formating

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import App from '@app/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from '@app/App';
 
 ReactDOM.render(
   <React.StrictMode>
