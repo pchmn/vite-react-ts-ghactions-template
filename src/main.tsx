d480b2199ac8f878f6112b75a207964314da3772
pchmn <pchmn.dev@gmail.com>
2022-04-18 17:07:43 +0200

    conf: update to react 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import App from '@app/App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
